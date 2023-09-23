5ef16eca0464d29b4b430a3c14c232abd947662f
MammadProMax <mhaagrm2@gmail.com>
2023-09-23 20:35:19 +0330

    first structure

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Home() {
+   return (
+      <>
+         <p className="text-3xl font-semibold">dashboard protected page</p>
+      </>
+   );
+}
