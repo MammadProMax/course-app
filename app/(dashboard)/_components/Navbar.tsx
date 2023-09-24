594182de346ba6231f510ac4166fe0b855ddc7d7
MammadProMax <mhaagrm2@gmail.com>
2023-09-24 13:00:51 +0330

    rename dir & add nav features

--- app/(dashboard)/_components/Navbar.tsx
+++ app/(dashboard)/_components/Navbar.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+
 import MobileSidebar from "./MobileSidebar";
+import NavbarRoutes from "@/components/global/NavbarRoutes";
 
 const Navbar = () => {
    return (
       <nav className="p-4 border-b h-full flex items-center bg-white shadow-sm">
          <MobileSidebar />
+         <NavbarRoutes />
       </nav>
    );
 };
